18164818fdf98626427505ecbf5170576713168f
johnpauls <johnpauls@operamail.com>
2015-06-08 16:17:53 -0400

    0.0.2  Add rudimentary controls

--- server.r
+++ server.r
@@ -4,10 +4,34 @@ library(NCRNbirds)
 NCRN<-importNCRNbirds("./Data/")
 shinyServer(function(input,output){
 
-  BMap<-leaflet() %>%
-    addTiles("//{s}.tiles.mapbox.com/v4/nps.2yxv8n84,nps.jhd2e8lb/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6IkdfeS1OY1UifQ.K8Qn5ojTw4RV1GwBlsci-Q") %>% addCircles(data=getPoints(object=NCRN))
+
+  MapColors<-reactive({
+    colorNumeric(palette=c("cyan","magenta4","orangered3"),domain=circleValues())
+  })
+  
+  ####### Circle Data 
+  
+  
+  ### based on input$mapvalues, get relevant data and add it to map
+  
+  circleValues<-reactive({
+    switch(input$mapValues,
+           nothing=c(1:10),
+           richness=1
+           #get the point names and count species.
+           )
     
-  output$BirdMap<-renderLeaflet(BMap)
+  })
+  
+  
+  output$BirdMap<-renderLeaflet({leaflet() %>%
+      
+      addTiles("//{s}.tiles.mapbox.com/v4/nps.2yxv8n84,nps.jhd2e8lb/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6IkdfeS1OY1UifQ.K8Qn5ojTw4RV1GwBlsci-Q") %>% 
+      
+      addCircles(data=getPoints(object=NCRN), color=MapColors()(circleValues()), fillColor=MapColors()(circleValues())) %>% 
+                   
+      addLegend(pal=MapColors(), values=circleValues(), title="test legend")
+  })
   
   
 })
--- ui.r
+++ ui.r
@@ -4,14 +4,18 @@ navbarPage(title="Bird Test", position = "static-top",inverse=TRUE, collapsible 
           
         tabPanel(title="Map", 
                  
-                 div(class="outer",
-                     tags$head(includeCSS("./www/mapstyles.css") ), # defines css file
-                 leafletOutput("BirdMap", width = "100%", height = "100%") #,width = "100%", height = "100%")
+                  div(class="outer",
+                      tags$head(includeCSS("./www/mapstyles.css") ), # defines css file that lets map take up whole screen
+                  leafletOutput("BirdMap", width = "100%", height = "100%")
+                  ),
+                 
+                 
+                 fixedPanel(id="controls", class="panel panel-primary", draggable = TRUE, cursor="auto", top="170px", bottom="auto",
+                            height="auto", right="120px", left="auto", width="200px", color="green",
+                            h4("Map Controls",class="panel-heading"),
+                            selectizeInput(inputId="mapValues", label="Data to Map", choices=c("Nothing Really"="nothing",
+                                                                                             "Species Numbers"="richness"))
                  )
-                 
-                 
-                 
-                 
-                 ),
+        ),
         tabPanel("test2", h2("test2"))
 )
