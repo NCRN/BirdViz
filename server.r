d8ba587c9ba903d8d005256e3fc63ae8fb4cf1a1
johnpauls <johnpauls@operamail.com>
2015-06-12 16:34:26 -0400

    0.0.3 many inmprovement, base map toggle, species numbers, started indivdual species work.

--- server.r
+++ server.r
@@ -1,37 +1,79 @@
 library(shiny)
 library(leaflet)
 library(NCRNbirds)
+library(dplyr)
 NCRN<-importNCRNbirds("./Data/")
-shinyServer(function(input,output){
+shinyServer(function(input,output,session){
 
 
-  MapColors<-reactive({
-    colorNumeric(palette=c("cyan","magenta4","orangered3"),domain=circleValues())
+  
+  ##### Set up Map #############
+  
+  output$BirdMap<-renderLeaflet({leaflet() %>%
+      #addTiles("//{s}.tiles.mapbox.com/v4/nps.2yxv8n84,nps.jhd2e8lb/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6IkdfeS1OY1UifQ.K8Qn5ojTw4RV1GwBlsci-Q") %>% 
+     setView(lng=-77.8,lat=39.03,zoom=9)
+  })
+
+  
+  ###toggles
+  observe({
+    toggle( id="SpeciesControls" , condition= (input$MapValues=="individual"))
+    toggleState( id='SpeciesValues', condition=( input$MapSpecies!=""))
   })
   
-  ####### Circle Data 
+  ### Reactive Map UI Widgets
   
+  observe({
+    updateSelectizeInput(session,inputId="MapSpecies",label="Species", choices=getChecklist(NCRN, years=input$MapYear), 
+                   options = list(placeholder='Choose a species'), server = TRUE
+    )
+  })
+  
+  ##### Choose BaseMap
+  observe({
+    leafletProxy("BirdMap") %>% 
+      clearTiles() %>% 
+      addTiles(urlTemplate= switch(input$MapBase,
+        Map="//{s}.tiles.mapbox.com/v4/nps.2yxv8n84,nps.jhd2e8lb/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6IkdfeS1OY1UifQ.K8Qn5ojTw4RV1GwBlsci-Q",
+        
+        Imagery="//{s}.tiles.mapbox.com/v4/nps.map-n9nxe12m,nps.gdipreks,nps.jhd2e8lb/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6IkdfeS1OY1UifQ.K8Qn5ojTw4RV1GwBlsci-Q"
+        )
+      )
+        
+    })
+
+  
+  
+  ####### Circle Data 
   
   ### based on input$mapvalues, get relevant data and add it to map
   
   circleValues<-reactive({
-    switch(input$mapValues,
-           nothing=c(1:10),
-           richness=1
-           #get the point names and count species.
-           )
+    switch(input$MapValues,
+          nothing=c(1:10),
+          richness={
+             X<-data.frame(Point_Name=getPoints(NCRN,years=input$MapYear)$Plot_Name) %>%      
+               group_by(Point_Name) %>% mutate(Richness=birdRichness(NCRN,points=Point_Name,years=input$MapYear))
+             as.vector(X$Richness)
+             },
+          individual=c(1:5)
+           
+    )
     
   })
   
-  
-  output$BirdMap<-renderLeaflet({leaflet() %>%
-      
-      addTiles("//{s}.tiles.mapbox.com/v4/nps.2yxv8n84,nps.jhd2e8lb/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibnBzIiwiYSI6IkdfeS1OY1UifQ.K8Qn5ojTw4RV1GwBlsci-Q") %>% 
-      
-      addCircles(data=getPoints(object=NCRN), color=MapColors()(circleValues()), fillColor=MapColors()(circleValues())) %>% 
-                   
-      addLegend(pal=MapColors(), values=circleValues(), title="test legend")
-  })
-  
+  ### Color funciton for circles
+  MapColors<-reactive({
+    colorNumeric(palette=c("cyan","magenta4","orangered3"),domain=circleValues())
+  })  
+
+  ### Update Map Circles and the legend
+       observe({
+         leafletProxy("BirdMap") %>%   
+         clearShapes() %>% 
+          addCircles(data=getPoints(object=NCRN,years=input$MapYear), color=MapColors()(circleValues()), 
+                fillColor=MapColors()(circleValues()),opacity=0.8, radius=50,fillOpacity = 0.8) %>% 
+          addLegend(layerId="CircleLegned",pal=MapColors(), values=circleValues(), title="test legend")
+        })
   
 })
--- ui.r
+++ ui.r
@@ -1,21 +1,48 @@
 library(shiny)
 library(leaflet)
+library(shinyjs)
+
 navbarPage(title="Bird Test", position = "static-top",inverse=TRUE, collapsible = FALSE, fluid=TRUE, windowTitle = "NCRN Birds",
           
         tabPanel(title="Map", 
-                 
+                 useShinyjs(),
                   div(class="outer",
                       tags$head(includeCSS("./www/mapstyles.css") ), # defines css file that lets map take up whole screen
                   leafletOutput("BirdMap", width = "100%", height = "100%")
                   ),
                  
+                 ################ Map Controls Box
                  
-                 fixedPanel(id="controls", class="panel panel-primary", draggable = TRUE, cursor="auto", top="170px", bottom="auto",
-                            height="auto", right="120px", left="auto", width="200px", color="green",
+                 fixedPanel( class="panel panel-primary controls", draggable = TRUE, cursor="auto", top="170px", bottom="auto",
+                            height="auto", right="120px", left="auto", width="200px",
                             h4("Map Controls",class="panel-heading"),
-                            selectizeInput(inputId="mapValues", label="Data to Map", choices=c("Nothing Really"="nothing",
-                                                                                             "Species Numbers"="richness"))
+                            selectizeInput(inputId="MapValues", label="Data to Map", choices=c("Nothing Really"="nothing",
+                                                                                             "Species Numbers"="richness",
+                                                                                              "Individual Species"="individual")
+                            ),
+                            
+                            div(id="SpeciesControls", 
+                               selectizeInput(inputId="MapSpecies",choices=NULL,label="Species"), #updated in server.r
+                               selectizeInput(inputId="SpeciesValues", label="Data", 
+                                              choices=c("Visit 1", "Visit 2", "Maximum Observed"))
+                            ),
+                            
+                            
+                            sliderInput(inputId="MapYear", label="Select a year:", min=2007,max=2014,
+                                         value=2014, step=1,sep="",round=TRUE)
+                 ),
+                 
+                 
+                 
+                 
+                 
+                 
+                 fixedPanel(class="panel panel-primary controls", draggable=TRUE, cursor="auto", top="85%", bottom="auto",
+                            height="auto", left="120px", width="180px",
+                            strong("Base Layer"),
+                            radioButtons(inputId="MapBase",label=NULL,choices=c("Map","Imagery"), selected="Map", inline=TRUE)
                  )
+                  
         ),
-        tabPanel("test2", h2("test2"))
+        tabPanel("Map Data", h2("test2"))
 )
